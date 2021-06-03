f647d064c635b74c1b49f1101c204f1c82da9c35
blackran <41161513+blackran@users.noreply.github.com>
2021-06-03 21:29:55 +0000

    Deploying to gh-pages from  @ 7b4a2ffb3e26ffecb38c48e4cb7dbf19fc5afbc1 🚀

--- precache-manifest.3082ac4a241a3b9393adbfbc4c3e04e4.js
+++ precache-manifest.3082ac4a241a3b9393adbfbc4c3e04e4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9ef51b276ce91cae7b8146aec17ccf69",
+    "revision": "50474d9f482475c8460534aada95fc50",
     "url": "/akanga/index.html"
   },
   {
-    "revision": "10e48aa90f8ace386325",
+    "revision": "b12f7cc6fed87bae76fb",
     "url": "/akanga/static/css/main.22f8c23c.chunk.css"
   },
   {
-    "revision": "5b806a49262818199e7b",
-    "url": "/akanga/static/js/2.5245643a.chunk.js"
+    "revision": "7e3c2bb652b30c2d1f98",
+    "url": "/akanga/static/js/2.1f3d717d.chunk.js"
   },
   {
-    "revision": "10e48aa90f8ace386325",
-    "url": "/akanga/static/js/main.ab813df7.chunk.js"
+    "revision": "b12f7cc6fed87bae76fb",
+    "url": "/akanga/static/js/main.c93dbfd5.chunk.js"
   },
   {
     "revision": "c54407b4f6db41b23112",
