383c2b9a466fad2700fc879bb381b2da4bc06a50
blackran <41161513+blackran@users.noreply.github.com>
2021-06-03 08:11:13 +0000

    Deploying to gh-pages from  @ 639003d33a293da665cc42d82b9c42a6968272da 🚀

---
+++ precache-manifest.9fd93a89323f0befd9426d33dccff93b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce554366b0c7cb2321a063b65d16e5fb",
+    "url": "/akanga/index.html"
+  },
+  {
+    "revision": "641679bcd66205090e68",
+    "url": "/akanga/static/css/main.22f8c23c.chunk.css"
+  },
+  {
+    "revision": "5b806a49262818199e7b",
+    "url": "/akanga/static/js/2.5245643a.chunk.js"
+  },
+  {
+    "revision": "641679bcd66205090e68",
+    "url": "/akanga/static/js/main.143c1fa4.chunk.js"
+  },
+  {
+    "revision": "c54407b4f6db41b23112",
+    "url": "/akanga/static/js/runtime-main.99b67b0e.js"
+  },
+  {
+    "revision": "3793c546e569b9f3fffabc07de0c6c19",
+    "url": "/akanga/static/media/akanga.3793c546.svg"
+  },
+  {
+    "revision": "9c34e6fc8e08c8648c5cc92979fd22a0",
+    "url": "/akanga/static/media/icon.9c34e6fc.jpeg"
+  },
+  {
+    "revision": "26537299b2372c9d4391f48ab176afa7",
+    "url": "/akanga/static/media/realisation.26537299.png"
+  },
+  {
+    "revision": "c71629e0f7b392e4138025b26148c659",
+    "url": "/akanga/static/media/sary.c71629e0.jpg"
+  },
+  {
+    "revision": "cd96b4e5a716e59f32c816670e635502",
+    "url": "/akanga/static/media/undraw_project_team_lc5a.cd96b4e5.svg"
+  }
+]);
