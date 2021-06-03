7b4a2ffb3e26ffecb38c48e4cb7dbf19fc5afbc1
blackran <andrianantenainarasolondraibe@gmail.com>
2021-06-04 00:27:38 +0300

    change BrowserRouter to HashRouter 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 
 // my import
