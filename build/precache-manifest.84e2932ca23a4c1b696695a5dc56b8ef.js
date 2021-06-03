8d339c9255ef9b425d2cffd39ec171fa4ffd360d
blackran <andrianantenainarasolondraibe@gmail.com>
2021-06-03 09:33:04 +0300

    first commit

---
+++ build/precache-manifest.84e2932ca23a4c1b696695a5dc56b8ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0ffad88edae5ec3e4111e2773532d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f811744141d18be31c38",
+    "url": "./static/css/main.9cc843cb.chunk.css"
+  },
+  {
+    "revision": "5ae6e1149d9c95658ba5",
+    "url": "./static/js/2.f8239efb.chunk.js"
+  },
+  {
+    "revision": "f811744141d18be31c38",
+    "url": "./static/js/main.6c8d02b1.chunk.js"
+  },
+  {
+    "revision": "76d136c86933e2957667",
+    "url": "./static/js/runtime-main.3882a19a.js"
+  },
+  {
+    "revision": "685757b1cd056592ec5f466ccc75a1f9",
+    "url": "./static/media/bg3-home.685757b1.jpg"
+  },
+  {
+    "revision": "707479611fdf7c612e6beaf8590398a2",
+    "url": "./static/media/bg_auth.70747961.jpg"
+  },
+  {
+    "revision": "7386969c5105286e46b6659a02b8b4e1",
+    "url": "./static/media/carte.7386969c.png"
+  },
+  {
+    "revision": "cedd52491ecd4712dec8454c808a3e8f",
+    "url": "./static/media/headerBg.cedd5249.png"
+  },
+  {
+    "revision": "8d253ba502f44679f83c6a4650d6b98e",
+    "url": "./static/media/libertain.8d253ba5.jpg"
+  },
+  {
+    "revision": "a8031d0a79b80c0a7d7062821aad1641",
+    "url": "./static/media/love.a8031d0a.jpg"
+  },
+  {
+    "revision": "4b42349dfd07b6aa5597f4448dfeef54",
+    "url": "./static/media/photoDeProfile.4b42349d.png"
+  },
+  {
+    "revision": "a48897c718850b8218f227feb0fd10c7",
+    "url": "./static/media/profil.a48897c7.png"
+  },
+  {
+    "revision": "65f898cc98edcdb11263115460906adc",
+    "url": "./static/media/sansLendemain.65f898cc.jpg"
+  }
+]);
