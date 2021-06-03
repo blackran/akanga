4578bbba22229aa17f5f4923ed4f17cd57916dee
blackran <41161513+blackran@users.noreply.github.com>
2021-06-03 08:06:46 +0000

    Deploying to gh-pages from  @ 9303f8543f75307a52380d69a695ef878e4eea85 🚀

---
+++ precache-manifest.bc3623812ca277149c2ac806074484d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c08e3ac32abae08c412912df86ceca5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb4c9e9c6e08fae5480",
+    "url": "/static/css/main.22f8c23c.chunk.css"
+  },
+  {
+    "revision": "5b806a49262818199e7b",
+    "url": "/static/js/2.5245643a.chunk.js"
+  },
+  {
+    "revision": "cfb4c9e9c6e08fae5480",
+    "url": "/static/js/main.50d7f1e9.chunk.js"
+  },
+  {
+    "revision": "a6b2edf39e4284d4214c",
+    "url": "/static/js/runtime-main.0f64b624.js"
+  },
+  {
+    "revision": "3793c546e569b9f3fffabc07de0c6c19",
+    "url": "/static/media/akanga.3793c546.svg"
+  },
+  {
+    "revision": "9c34e6fc8e08c8648c5cc92979fd22a0",
+    "url": "/static/media/icon.9c34e6fc.jpeg"
+  },
+  {
+    "revision": "26537299b2372c9d4391f48ab176afa7",
+    "url": "/static/media/realisation.26537299.png"
+  },
+  {
+    "revision": "c71629e0f7b392e4138025b26148c659",
+    "url": "/static/media/sary.c71629e0.jpg"
+  },
+  {
+    "revision": "cd96b4e5a716e59f32c816670e635502",
+    "url": "/static/media/undraw_project_team_lc5a.cd96b4e5.svg"
+  }
+]);
