342df63fd08ac999ed02b6cde7033648fc282dcd
blackran <andrianantenainarasolondraibe@gmail.com>
2021-06-03 10:01:38 +0300

    Updates

---
+++ precache-manifest.73ce428e45a21b6a5f079006d2a737df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11e9d10a92e935ab95e34b6069b3723",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09784b39b74c83d7c2e6",
+    "url": "./static/css/main.22f8c23c.chunk.css"
+  },
+  {
+    "revision": "832f458c6e9320662cd4",
+    "url": "./static/js/2.7f9aabea.chunk.js"
+  },
+  {
+    "revision": "09784b39b74c83d7c2e6",
+    "url": "./static/js/main.de03f190.chunk.js"
+  },
+  {
+    "revision": "76d136c86933e2957667",
+    "url": "./static/js/runtime-main.3882a19a.js"
+  },
+  {
+    "revision": "d7059fd4da041cc34e1082e89e521bec",
+    "url": "./static/media/akangfa.d7059fd4.png"
+  },
+  {
+    "revision": "9c34e6fc8e08c8648c5cc92979fd22a0",
+    "url": "./static/media/icon.9c34e6fc.jpeg"
+  },
+  {
+    "revision": "26537299b2372c9d4391f48ab176afa7",
+    "url": "./static/media/realisation.26537299.png"
+  },
+  {
+    "revision": "c71629e0f7b392e4138025b26148c659",
+    "url": "./static/media/sary.c71629e0.jpg"
+  },
+  {
+    "revision": "cd96b4e5a716e59f32c816670e635502",
+    "url": "./static/media/undraw_project_team_lc5a.cd96b4e5.svg"
+  }
+]);
